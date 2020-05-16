1069a85f6fc00a2a27c0605e91799f323fa81ee8
LucasGois <lucas.a.gois98@gmail.com>
2020-05-16 17:35:33 -0300

    add produto, categorias produtos

---
+++ app/Produto.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    protected $table = "produtos";
+    protected $primaryKey = "id";
+
+    public function categorias() {
+        return $this->belongsToMany('App\Categoria', 'categorias_produtos', 'id_produto', 'id_categoria')->withTimestamps();
+    }
+}
