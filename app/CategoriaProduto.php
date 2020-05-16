8a92c0ddf0e677f85a2b7d1501998c3aa0cf07f0
LucasGois <lucas.a.gois98@gmail.com>
2020-05-16 14:31:41 -0300

    add model CategoriaProduto

---
+++ app/CategoriaProduto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriaProduto extends Model
+{
+    protected $table = "categorias_produto";
+    protected $primaryKey = "id";
+}
