806efa9971943ff0796cbb48a9de239e0402a4f3
LucasGois <lucas.a.gois98@gmail.com>
2020-05-18 20:37:20 -0300

    atualização da view cidade_lista

--- resources/views/cidade/lista.blade.php
+++ resources/views/cidade/lista.blade.php
@@ -3,37 +3,42 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
 
-            <div class="table-wrapper">
+            <div class="card">
 
-                <div class="table-title">
-                    <div class="row">
-                        <div class="col-sm-10"><h2>Lista de <b>Cidades</b></h2></div>
-                        <div class="col-sm-2 text-right">
-                            <button type="button" class="btn btn-success">Add</button>
-                        </div>
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-10">
+                        <h2>
+                            Lista de <b>Cidades</b>
+                        </h2>
+                    </div>
+                    <div class="col-2 text-right">
+                        <button type="button" class="btn btn-success">Add</button>
                     </div>
                 </div>
-
-                <table class="table table-bordered">
+            </div>
+            <div class="card-body">
+            
+                <table class="table">
                     <thead>
-                        <tr class="d-flex">
-                            <th class="col-1" scope="col">ID</th>
-                            <th class="col-5" scope="col">Nome</th>
-                            <th class="col-4" scope="col">Estado</th>
-                            <th class="col-2" scope="col">Ações</th>
+                        <tr class="row">
+                            <th class="text-center col-sm-2 col-md-1">ID</th>
+                            <th class="text-center col-sm-5 col-md-6">Nome</th>
+                            <th class="text-center col-sm-3 col-md-3">Estado</th>
+                            <th class="text-center col-sm-2 col-md-2" >Ações</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($cidades as $cidade)
-                            <tr class="d-flex">
-                                <th class="col-1 align-middle" scope="row">{{ $cidade->id }}</td>
-                                <td class="col-5 align-middle">{{ $cidade->nome }}</td>
-                                <td class="col-4 align-middle">{{ $cidade->estado }}</td>
-                                <td class="col-2 align-middle">
-                                    <button type="button" class="align-middle btn btn-sm btn-warning">Alterar</button>
-                                    <button type="button" class="align-middle btn btn-sm btn-danger">Excluir</button>
+                            <tr class="row">
+                                <th class="text-center col-sm-2 col-md-1">{{ $cidade->id }}</td>
+                                <td class="text-center col-sm-5 col-md-6">{{ $cidade->nome }}</td>
+                                <td class="text-center col-sm-3 col-md-3">{{ $cidade->estado }}</td>
+                                <td class="text-center col-sm-2 col-md-2">
+                                    <button type="button" class="btn btn-sm btn-warning">Alterar</button>
+                                    <button type="button" class="btn btn-sm btn-danger">Excluir</button>
                                 </td>
                             </tr>
                         @endforeach
@@ -41,6 +46,8 @@
                 </table>
                 
                 {{ $cidades->links() }}
+
+            </div>
 
             </div>
 
