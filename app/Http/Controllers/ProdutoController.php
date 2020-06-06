705a99657c796a9e3559f6dcaa2621773f7541cf
MarcosGoldblip <0801MarcosGoldblip@users.noreply.github.com>
2020-06-06 16:51:59 -0300

    Falta completar o upload de imagem

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -31,6 +31,74 @@ class ProdutoController extends Controller{
         ]);
     }
 
+     public function cadastro($id = 0) {
+
+        if($id > 0){
+            // Alterar
+            $produto = Produto::find($id);
+        } else {
+            // Adicionar
+            $produto = new Produto();
+        }
+
+       // $cidades = Cidade::all();
+        
+        return view('produto.cadastro', [
+            'produto' => $produto,
+            //'cidades'=>$cidades
+        ]);
+    }
+
+    public function salvar(Request $req, $id = 0) {
+
+        $req->validate([
+            'nome' => 'required|min:3|unique:cidades,nome',
+            'valor' => 'required|numeric',
+            
+        ]);
+
+        if($id > 0){
+            $produto = Produto::find($id);
+        } else {
+            $produto = new Produto();
+        }
+
+        $imagem = $req->file('upload');
+        $nome = $req->input('nome');
+    
+        
+        $produto->nome = $req->input('nome');
+        $produto->descricao = $req->input('descricao');
+        $produto->estoque = $req->input('estoque');
+        $produto->valor = $req->input('valor');
+        $produto->imagem = $nome;
+
+        $produto->save();
+
+        $slug = $produto->nome . " " . $produto->id;
+        $slug = Str::of($slug)->slug('-');
+        $slug = $slug . "." . $imagem->extension();
+
+        $slug = $imagem->storeAs('imagens_produtos', $slug);
+
+        $produto->imagem = "upload/$slug";
+        if ($produto->save()){
+            $msg = "Produto adicionado!";
+        } else {
+            $msg = "Produto não foi cadastrado. Tente novamente!!";
+        }
+
+
+        return redirect()->route('produto_lista');
+    }
+
+
+    public function excluir($id){
+        $produto = Produto::find($id);
+        $produto->delete();
+        return redirect()->route('produto_lista');
+    }
+
     public function add_carrinho($id)
     {
         $variavel = [
