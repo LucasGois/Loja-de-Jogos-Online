cfa637dfa0843ee8e92ba6b29dcf32e180f47cdb
MarcosGoldblip <0801MarcosGoldblip@users.noreply.github.com>
2020-06-08 22:48:57 -0300

    teste home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,40 +5,42 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
 
-                <div class="card mt-3">
-                    <div class="card-header"><h2><b>Menu</b></h2></div>
-
-                    <div class="card-body">
-
-                        <!-- Produtos -->
-                        <div class="col col-md-12 text-right">
-                            <a href="{{ route('produto_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Produtos</a>
-                        </div>
-
-                        @if ($admin ?? false)
-
-                            <!-- Cidades -->
-                            <div class="col col-md-12 text-right">
-                                <a href="{{ route('cidade_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Cidades</a>
-                            </div>
-
-                            <!-- Endereços -->
-                            <div class="col col-md-12 text-right">
-                                <a href="{{ route('endereco_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Endereços</a>
-                            </div>
-
-                            <!-- Categorias -->
-                            <div class="col col-md-12 text-right">
-                                <a href="{{ route('categoria_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Categorias</a>
-                            </div>
-
-                        @endif
-
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                      <h2 class="mb-0">
+                        <button class="btn btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          <b><h2>Menu</h2></b>
+                        </button>
+                      </h2>
                     </div>
 
-                </div>
+                    <!-- Produtos -->
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample"> 
+                        <a href="{{ route('produto_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Produtos</a>   
+                    </div>
 
+                    @if ($admin ?? false)
+
+                    <!-- Cidades -->
+                    <div class="accordion" id="accordionExample"> 
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample"> 
+                            <a href="{{ route('cidade_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Cidades</a>   
+                    </div>
+
+                    <!-- Endereços -->
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample"> 
+                        <a href="{{ route('endereco_lista') }}" class="btn btn-outline-secondary btn-lg btn-block">Endereços</a>   
+                    </div>
+
+                    <!-- Categorias -->
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample"> 
+                            <a href="{{ route('categoria_lista') }}" class="btn btn-outline-secondary btn-lg btn-block ">Categorias</a>    
+                        </div>
+                    </div>
+                </div>
+                    @endif
             </div>
-        </div>
+         </div>
     </div>
+        
 @endsection
