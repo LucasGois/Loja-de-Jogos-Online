a673e1a1260c1fc37d804bd96f0f5ed7e4cfecf3
MarcosGoldblip <0801MarcosGoldblip@users.noreply.github.com>
2020-06-29 19:13:12 -0300

    teste readme

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,7 +51,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeypress="$(this).mask('000.000.000-00');" >
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -150,4 +150,5 @@
         </div>
     </div>
 </div>
+
 @endsection
